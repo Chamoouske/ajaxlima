0f46911d822f2a60a33c0c12d4bb7f2c4f31572e
Ajax Lima <ajaxlima94@gmail.com>
2021-12-04 16:07:27 -0300

    Fix[theme]: englobe all <p> into a <div> for align left

--- src/components/links/index.tsx
+++ src/components/links/index.tsx
@@ -13,45 +13,50 @@ import styles from './styles.module.scss';
 export function Links() {
   return (
     <ContentBox>
-      <p className={styles.textLink}>
-        <img src={MapPin} />
-        Brasil
-      </p>
+      <div className={styles.content}>
+        <p className={styles.textLink}>
+          <img src={MapPin} />
+          Brasil
+        </p>
 
-      <p className={styles.textLink}>
-        <img src={Briefcase} />
-        Petruz
-      </p>
+        <p className={styles.textLink}>
+          <img src={Briefcase} />
+          Petruz
+        </p>
 
-      <p className={styles.textLink}>
-        <img src={Github} />
-        <a href="https://github.com/Chamoouske" target="_blank">
-          Chamoouske
-        </a>
-      </p>
+        <p className={styles.textLink}>
+          <img src={Github} />
+          <a href="https://github.com/Chamoouske" target="_blank">
+            Chamoouske
+          </a>
+        </p>
 
-      <p className={styles.textLink}>
-        <img src={Instagram} />
-        <a href="https://www.instagram.com/ajaxlima/" target="_blank">
-          Instagram
-        </a>
-      </p>
+        <p className={styles.textLink}>
+          <img src={Instagram} />
+          <a href="https://www.instagram.com/ajaxlima/" target="_blank">
+            Instagram
+          </a>
+        </p>
 
-      <p className={styles.textLink}>
-        <img src={Linkedin} />
-        <a href="https://www.linkedin.com/in/AjaxLima" target="_blank">
-          LinkedIn
-        </a>
-      </p>
+        <p className={styles.textLink}>
+          <img src={Linkedin} />
+          <a href="https://www.linkedin.com/in/AjaxLima" target="_blank">
+            LinkedIn
+          </a>
+        </p>
 
-      <p className={styles.textLink}>
-        <img src={Globe} />
-      </p>
+        <p className={styles.textLink}>
+          <img src={Globe} />
+          <a href="https://ajaxlima.vercel.app" target="_blank">
+            https://ajaxlima.vercel.app
+          </a>
+        </p>
 
-      <p className={styles.textLink}>
-        <img src={Mail} />
-        ajaxlima94@gmail.com
-      </p>
+        <p className={styles.textLink}>
+          <img src={Mail} />
+          ajaxlima94@gmail.com
+        </p>
+      </div>
     </ContentBox>
   );
 }
