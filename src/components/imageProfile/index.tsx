c8b26bf55e4ffe748bafe61e3dd22f8fdb2d293d
Ajax Lima <ajaxlima94@gmail.com>
2021-12-04 15:54:37 -0300

    Create[imageProfile]: index.tsx + styles.modules.scss

---
+++ src/components/imageProfile/index.tsx
@@ -0,0 +1,12 @@
+import styles from './styles.module.scss';
+
+export function ImageProfile() {
+  return (
+    <img
+      src="https://github.com/Chamoouske.png"
+      className={styles.imageProfile}
+      alt="Ajax Lima's Github profile image"
+      title="Ajax Lima"
+    />
+  );
+}
