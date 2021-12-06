58c4af429233d0d449e35d14a8cef41f3a425c6c
Ajax Lima <ajaxlima94@gmail.com>
2021-12-06 19:43:25 -0300

    Add[home]:component myProject in home screen

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,24 @@
 import { Profile } from '../../components/profile';
 import { Links } from '../../components/links';
 import { Technologies } from '../../components/technologies';
+import { MyProjects } from '../../components/myProjects';
 
 import styles from './styles.module.scss';
 
 export function Home() {
   return (
     <div className={styles.container}>
-      <Profile />
-      <Links />
-      <Technologies />
+      <div className={styles.containerInfo}>
+        <Profile />
+
+        <Links />
+
+        <Technologies />
+      </div>
+
+      <div className={styles.containerSocialMidia}>
+        <MyProjects />
+      </div>
     </div>
   );
 }
