1ec1a9e996328b7cc9d05c094031a1c6694c4dbe
Ajax Lima <ajaxlima94@gmail.com>
2021-12-14 18:36:03 -0300

    Create[Experience]:component + scss

---
+++ src/components/experience/index.tsx
@@ -0,0 +1,41 @@
+import { ContentBox } from '../contentBox';
+
+import styles from './styles.module.scss';
+
+type ExperienceT = {
+  local: string;
+  timeCourse: string;
+  occupation: string;
+};
+
+export function Experience() {
+  const experiences: any = [
+    {
+      local: 'Petruz',
+      timeCourse: '07/2021 - Today',
+      occupation: 'Infrastructure intern',
+    },
+  ];
+
+  return (
+    <ContentBox>
+      <div className={styles.container}>
+        <h2 className={styles.title}>Experiências</h2>
+
+        <div className={styles.experiencesContainer}>
+          {experiences.map((experience: ExperienceT, i: any) => (
+            <div className={styles.experience} key={i}>
+              <li>
+                {experience.local}
+                <div>
+                  <span>{experience.timeCourse}</span>
+                  <span>{experience.occupation}</span>
+                </div>
+              </li>
+            </div>
+          ))}
+        </div>
+      </div>
+    </ContentBox>
+  );
+}
